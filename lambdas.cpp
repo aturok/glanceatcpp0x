04ce4b511860eaac52718ff7eb08024090a04ac8
Alexander Turok <chaot8@gmail.com>
2013-02-24 02:02:29 +0400

    Added square lambda. Tryied it in for loop.

--- lambdas.cpp
+++ lambdas.cpp
@@ -9,5 +9,13 @@ int main(int argc, char** argv)
 
 	cout << "Hello, " << name << "!" << endl;
 
+
+	auto square = [](int a){return a*a;};
+
+	for(int i = 0; i < 10; ++i)
+	{
+		cout << i << " squared is " << square(i) << endl;
+	}
+
 	return 0;
 }
